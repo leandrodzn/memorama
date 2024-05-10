95f037a26819c14725e59ad7468b51d9d5d45c9c
Leandro Dzib <leandrodzibn@gmail.com>
2024-05-10 10:17:46 -0600

    add variables

--- core/php/DataBaseManager.php
+++ core/php/DataBaseManager.php
@@ -6,10 +6,10 @@
  * Date: 07/02/2016
  * Time: 07:55 PM
  */
-define('SERVER', 'localhost');
-define('USERNAME', 'root');
-define('PASSWORD', 'liverpool');
-define('DB', 'memorama');
+// define('SERVER', 'localhost');
+// define('USERNAME', 'root');
+// define('PASSWORD', 'liverpool');
+// define('DB', 'memorama');
 
 class DataBaseManager {
 
@@ -20,7 +20,13 @@ class DataBaseManager {
      * DataBaseManager constructor.
      */
     private function __construct() {
-        $this->mysqli = new mysqli(SERVER, USERNAME, PASSWORD, DB);
+        $server = getenv('DB_SERVER');
+        $username = getenv('DB_USERNAME');
+        $password = getenv('DB_PASSWORD');
+        $db = getenv('DB_NAME');
+
+        $this->mysqli = new mysqli($server, $username, $password, $db);
+        // $this->mysqli = new mysqli(SERVER, USERNAME, PASSWORD, DB);
         if ($this->mysqli->connect_errno) {
             echo "Fallo al conectar a MySQL: (" . $this->mysqli->connect_errno . ") " . $this->mysqli->connect_error;
         }
